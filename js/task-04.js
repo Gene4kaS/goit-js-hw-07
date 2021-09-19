12c3d08b6ab6ab51274c247d54987adb7fbae396
Gene4kaS <jannasavchenko@gmail.com>
2021-09-19 21:33:22 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,20 @@
 const button = {
     plus: document.querySelector("[data-action='increment']"),
     minus: document.querySelector("[data-action='decrement']"),
+    counter: document.querySelector('#value'),
   };
   let counterValue = 0;
   const increment = () => {
     counterValue += 1;
-  
-    document.getElementById('value').textContent = counterValue;
+    // document.getElementById('value').textContent = counterValue;
+    button.counter.textContent = counterValue;
   };
   
   const decrement = () => {
     counterValue -= 1;
-  
-    document.getElementById('value').textContent = counterValue;
+    // document.getElementById('value').textContent = counterValue;
+    button.counter.textContent = counterValue;
   };
-  
+
   button.plus.addEventListener('click', increment);
   button.minus.addEventListener('click', decrement);
