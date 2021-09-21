208f24b523e3249e84ac4548da06117cdf13a132
Gene4kaS <jannasavchenko@gmail.com>
2021-09-21 14:21:18 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,15 @@
 
 
+
 const input = document.getElementById("validation-input");
+    input.addEventListener("blur", OnInputBlur);
 
-const addRemoveColor = (remove, add) => {
-  input.classList.remove(`${remove}`);
-  input.classList.add(`${add}`);
-};
-
-const OnInputBlur = (e) => {
-  if (Number(input.dataset.length) === e.currentTarget.value.length) {
-    addRemoveColor("invalid", "valid");
-  } else {
-    addRemoveColor("valid", "invalid");
-  }
-};
-
-input.addEventListener("blur", OnInputBlur);
+    function OnInputBlur() {
+        var correctLength = Number(this.dataset.length);
+        var inputDataLength = this.value.length;
+        if (correctLength === inputDataLength) {
+            this.classList = 'valid';
+        } else {
+            this.classList = 'invalid';
+        } 
+    }
